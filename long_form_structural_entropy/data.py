bc7dd3c42a5c99405688b77d14e4e868969309c7
Xingtao Zhao <129815976+XingTao-sole@users.noreply.github.com>
2025-11-10 20:48:22 +0800

    Update data.py

--- long_form_structural_entropy/data.py
+++ long_form_structural_entropy/data.py
@@ -1,1 +1,5 @@
-
+# For long-form generation experiments, we evaluate on two challenging datasets featuring real-world entities from Wikipedia: FActScore and PopQA . 
+# To ensure data quality, we exclude entities with multiple Wikipedia entries or those with pages shorter than 2,000 tokens. 
+# For each dataset, we randomly sample 100 entities and generate a set of claims for each entity. This process yields over 1800 labeled claims (true or false) 
+# on average for each model-dataset combination. 
+# The long-form data will be made open source after the article is accepted.
